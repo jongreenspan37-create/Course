06f00aa2952ef8167c5dcd97243c6fd0674adc7b
jongreenspan37-create <jongreenspan37@gmail.com>
2026-05-09 11:45:06 +0100

    Contiued development of banking app JAV01 3

--- JAV01/Module3/index.js
+++ JAV01/Module3/index.js
@@ -1,10 +1,11 @@
 import {bankAccount} from './bankAccount.js';
 
-//Create an account
-const oneId = new bankAccount("Jon", "BY0001","password", 0)
-console.log(oneId)
+//Create an account variable
+
 let openAcc = null;
-//Create an account
+
+
+//Create an account from form
 const myForm = document.getElementById('accCreate');
 
 myForm.addEventListener('submit', (e) =>{
@@ -14,11 +15,21 @@ myForm.addEventListener('submit', (e) =>{
     //variable for class
     const nameValue = document.getElementById('name').value;
     const passValue = document.getElementById('pass').value;
-    const accValue = createAccno();
+    
+    //Validate inputs
+    if(nameValue.trim().length === 0 || passValue.trim().length === 0){
+        console.log("No name added")
+        return;
+    }
+
+
+    
+    const accValue = createAccno(); //call function to generate random account no
     console.log(accValue);
     console.log(nameValue);
     console.log(passValue);
      
+    
     //create new object from class
     openAcc = new bankAccount(nameValue,accValue,passValue,0);
     console.log(openAcc);
@@ -27,11 +38,7 @@ myForm.addEventListener('submit', (e) =>{
 
 //generates a random account no
 function createAccno(){
-    let accNo=""
-    for (let i = 0; i < 10; i++){
-        accNo += Math.floor(Math.random() * 10);
-    }
-    return accNo;
+    return Math.random().toString().slice(2,12).padEnd(9,'0');
 }
 
 //Displays account details and creates table entries
@@ -39,6 +46,22 @@ function displayAccount(){
     console.log(openAcc)
     const displayDiv = document.getElementById('account-details');
     if(openAcc){
-    displayDiv.innerHTML = `<Strong>Name: </strong>${openAcc.name}<strong><span style = "margin-left:15px;" >Account No :</strong>${openAcc.account}</span>`;
+    displayDiv.innerHTML = `<Strong>Name: </strong>${openAcc.name}<strong>
+            <span style = "margin-left:15px;" >Account No :</strong>${openAcc.account}</span>`;
     }
 }
+
+const passInput = document.getElementById('pass');
+const toggleIcon = document.getElementById('pass-toggle');
+
+// Show password on hover
+toggleIcon.addEventListener('mouseenter', () => {
+    passInput.type = 'text';
+    toggleIcon.classList.replace('fa-eye', 'fa-eye-slash');
+});
+
+// Hide password when mouse leaves
+toggleIcon.addEventListener('mouseleave', () => {
+    passInput.type = 'password';
+    toggleIcon.classList.replace('fa-eye-slash', 'fa-eye');
+});
