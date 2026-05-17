c95ad9239adda3e84369e94ed734e6648ff3115c
jongreenspan37-create <jongreenspan37@gmail.com>
2026-05-17 21:55:00 +0100

    Developing layout three and adding links to other pages

---
+++ HAW01/Basic layouts/scripts/layout3.js
@@ -0,0 +1,75 @@
+const leftBtn = document.getElementById('left-btn');
+        const rightBtn = document.getElementById('right-btn');
+        const carouselTrack = document.getElementById('carousel-track');
+
+        const dot1 = document.getElementById('slide-1');
+        const dot2 = document.getElementById('slide-2');
+        const dot3 = document.getElementById('slide-3');
+
+        let currentSlideIndex = 0;
+
+        function updateCarouselPosition(move) {
+    
+            const amountToMove = move * 512;
+    
+            carouselTrack.style.transform = `translateX(-${amountToMove}px)`;
+
+            
+            
+            // 3. Reset all dots to the inactive slate color first
+            dot1.className = "h-4 w-4 rounded-full bg-slate-500";
+            dot2.className = "h-4 w-4 rounded-full bg-slate-500";
+            dot3.className = "h-4 w-4 rounded-full bg-slate-500";
+
+            // 4. Give only the active dot the bright blue color based on the 'move' number
+            if (move === 0) dot1.className = "h-4 w-4 rounded-full bg-blue-800";
+            if (move === 1) dot2.className = "h-4 w-4 rounded-full bg-blue-800";
+            if (move === 2) dot3.className = "h-4 w-4 rounded-full bg-blue-800";
+        }
+
+        rightBtn.addEventListener('click', () => {
+    
+            if (currentSlideIndex < 2) {
+            currentSlideIndex++;
+           
+            } else{
+                currentSlideIndex = 0;
+            }
+
+            console.log("Current Slide Index:", currentSlideIndex);
+            updateCarouselPosition(currentSlideIndex);
+            
+        });
+
+        
+
+
+        leftBtn.addEventListener('click', () => {
+    
+            if (currentSlideIndex > 0) {
+                currentSlideIndex--;
+            
+            }else{
+                currentSlideIndex = 2;
+            }
+
+            console.log("Current Slide Index:", currentSlideIndex); 
+
+            updateCarouselPosition(currentSlideIndex);
+
+        });
+
+        dot1.addEventListener('click', ()=>{
+            updateCarouselPosition(0);
+
+        });
+
+        dot2.addEventListener('click', ()=>{
+            updateCarouselPosition(1);
+
+        });
+
+        dot3.addEventListener('click', ()=>{
+            updateCarouselPosition(2);
+
+        });
